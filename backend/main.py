9e90989a2fdf195bc76d38ad9fe08f260a175c04
SamuelAchilike <samidope15@gmail.com>
2025-09-28 19:34:41 +0100

    Wrote simple FastAPI application to get and post transaction details

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,37 @@
-from solana.rpc.api import Client
+# from solana.rpc.api import Client
+#
+# http_client = Client("https://api.devnet.solana.com")
 
-http_client = Client("https://api.devnet.solana.com")
+from fastapi import FastAPI
+from typing import  List
+import random
+
+app = FastAPI
+
+transactions = []
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Test Tube API 🚀"}
+
+@app.post("/pay")
+def pay(user: str, amount: float):
+    tx_id = f"tx_{random.randint(1000, 9999)}"
+    transaction = {"id": tx_id, "user": user, "amount": amount, "status": "confirmed"}
+    transactions.append(transaction)
+    return {"message": "Payment Simulated", "transaction": transaction}
+
+@app.get("/transactions")
+def get_transactions():
+    return {"transactions": transactions}
+
+@app.get("analytics")
+def analytics():
+    total = len(transactions)
+    total_amount = sum(tx["amount"] for tx in transactions)
+    suspicious = [tx for tx in transactions if tx["amount"] > 1000]
+    return {
+        "total_transactions": total,
+        "total_amount": total_amount,
+        "suspicious": suspicious
+    }
